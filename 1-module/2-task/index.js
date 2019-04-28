230d7546e55b0b6672b3517aab258d4e876466d5
nobody nothing <moongangbanger@gmail.com>
2019-04-28 17:03:08 +0300

    fix?

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -13,7 +13,10 @@ function print (text) {
  */
  
 function isValid (name) {
-	if (name.length > 3 && name.indexOf(' ') == -1) return true;
+	if (name.length > 0) {
+		if (name.length > 3 && name.indexOf(' ') == -1) return true;
+		else return false;
+	}
 	else return false;
 }
 
