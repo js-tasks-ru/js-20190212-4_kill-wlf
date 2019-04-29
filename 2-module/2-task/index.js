3c4d7f8111f6af7be0d6d171875e5d2427f7bd00
nobody nothing <moongangbanger@gmail.com>
2019-04-29 21:13:01 +0300

    some fixes

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,5 @@
+'use script'
+
 /**
  * Клонируем объект
  * @param {Object} obj - объект в котором ищем
@@ -5,22 +7,24 @@
  * @returns {Object}
  */
 
-let arr = [];
-let str = '';
-
 function find (obj, value) {
+	let arr = [];
+	let str = '';
+	findRecur(obj, value);
 	
-	for (let key in obj){
-		if (typeof(obj[key]) === 'object') { //проходимся по ключам, если ключ это объект, то пускаем рекурсию в этот объект, попутно записывая начало адреса.
-			str += key+'.';
-			find(obj[key], value);
-		}
-		else if (obj[key] === value) { //если значение ключа равно искомому значению, то плюсуем остаток адреса к строке, пихаем строку в массив и обнуляем ее.
-			str += key;
-			arr.unshift(str);
-			str = '';
-		};	 	
-	};
+	function findRecur (obj, value) {
+		for (let key in obj){
+			if (typeof(obj[key]) === 'object') { //проходимся по ключам, если ключ это объект, то пускаем рекурсию в этот объект, попутно записывая начало адреса.
+				str += key+'.';
+				findRecur(obj[key], value);
+			}
+			else if (obj[key] === value) { //если значение ключа равно искомому значению, то плюсуем остаток адреса к строке, пихаем строку в массив и обнуляем ее.
+				str += key;
+				arr.unshift(str);
+				str = '';
+			};	 	
+		};
+	}
 	
 	if (arr.length == 1) return arr.join(''); //если только один элемент в массиве, то возвращаем строку
 	else if (arr.length == 0) return null; //нулл если ничего не нашли
