84c63e0f607ac219964dfbdde7538d24ab2fe094
nobody nothing <moongangbanger@gmail.com>
2019-05-02 20:54:58 +0300

    fixes

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -7,30 +7,32 @@
 function highlight(table) {
 	
 	const data = document.body.getElementsByTagName('td');
-	console.log(data.length);
 	
-	for ( let i = 0;  i < data.length; i++) {
-		
-		//dataset.available proverka
+	for (let i = 0; i < data.length; i++) {
+		//Проставит класс available/unavailable, в зависимости от значения атрибута data-available у ячейки Status
 		if (data[i].hasAttribute('data-available')) {
-			if (data[i].dataset.available === 'true') data[i].classList.add('available');
-			else data[i].classList.add('unavailable');
-		}	
-		else data[i].setAttribute('hidden', true);
+			if (data[i].dataset.available === 'true') data[i].parentElement.classList.add('available');
+			else data[i].parentElement.classList.add('unavailable');
+		}
 		
-		//sex proverka
+		//Проставит аттрибут hidden, если  data-available атрибута нет вообще
+		//if (data[i].hasAttribute('data-available') == false) data[i].parentElement.setAttribute('hidden', true);
+		
+		//Проставит класс male/female, в зависимости от содержимого ячекйки Gender
 		if (data[i].innerHTML == 'm') {
-			data[i].classList.add('Male');
+			data[i].parentElement.classList.add('Male');
 		}
 		else if (data[i].innerHTML == 'f') {
-			data[i].classList.add('Female');
+			data[i].parentElement.classList.add('Female');
 		}
 		
-		//vozrast proverka
+		//'Установит inline-стиль style="text-decoration: line-through", если значение ячейки Age меньше 18'
 		if (isFinite(data[i].innerHTML)) {
 			let num = data[i].innerHTML;
-			if (num < 18) data[i].style.textDecoration ='line-through';
+			if (num < 18) data[i].parentElement.style.textDecoration ='line-through';
 		}
+		
+		
 	}
 	
 }
