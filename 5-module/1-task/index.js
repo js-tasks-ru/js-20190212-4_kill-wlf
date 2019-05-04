bcf1d24a115d5d0bf8fdb9b88689e75f62417fa6
nobody nothing <moongangbanger@gmail.com>
2019-05-04 14:28:06 +0300

    fixes

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -11,8 +11,8 @@ function highlight(table) {
 	for (let i = 0; i < data.length; i++) {
 		//Проставит класс available/unavailable, в зависимости от значения атрибута data-available у ячейки Status
 		if (data[i].hasAttribute('data-available')) {
-			if (data[i].dataset.available === 'true') data[i].parentElement.classList.add('available');
-			else data[i].parentElement.classList.add('unavailable');
+			if (data[i].dataset.available === 'true') data[i].classList.add('available');
+			else data[i].classList.add('unavailable');
 		}
 		
 		//Проставит аттрибут hidden, если  data-available атрибута нет вообще
@@ -20,16 +20,16 @@ function highlight(table) {
 		
 		//Проставит класс male/female, в зависимости от содержимого ячекйки Gender
 		if (data[i].innerHTML == 'm') {
-			data[i].parentElement.classList.add('Male');
+			data[i].classList.add('male');
 		}
 		else if (data[i].innerHTML == 'f') {
-			data[i].parentElement.classList.add('Female');
+			data[i].classList.add('female');
 		}
 		
 		//'Установит inline-стиль style="text-decoration: line-through", если значение ячейки Age меньше 18'
 		if (isFinite(data[i].innerHTML)) {
 			let num = data[i].innerHTML;
-			if (num < 18) data[i].parentElement.style.textDecoration ='line-through';
+			if (num < 18) data[i].style.textDecoration ='line-through';
 		}
 		
 		
